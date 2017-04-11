3d2bb4219c7c374a2e80d789b7bf1ba3c82b2f3d
Rob Worth <Rob Worth@172.21.90.81>
2017-04-11 19:00:41 -0400

    First Draft of Main

--- Main.java
+++ Main.java
@@ -1,15 +1,37 @@
+
 //Robert Worth
 //OOP
 //J.Anderson
-//Team Project Coffee Machine
+//Team Project
+//Coffee Machine
 
 
 //Main
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
 
 
 public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			VBox root = FXMLLoader.load( 
+					getClass( ).getResource("coffee_machine.fxml"));
+					
+			Scene scene = new Scene(root,600,400);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle( "Coffee Machine");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
-	//enter code here
-
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
