4d2e00f52606809e19f8db0d7dfe7a01626d276a
dslgo <dslgo@LAPTOP-COVI3FBC>
2017-04-12 16:42:58 -0500

    Grace's revisions added.

--- Controller.java
+++ Controller.java
@@ -7,45 +7,65 @@
 	
 public class Controller {
 
-private double amountNumber;
-	
-	@FXML private Button blackCoffee, coffeeWithSugar, coffeeWithSugarAndCreamer, enter, go;
-	@FXML private Label headerText, currentSelection, amountPaid, change;
-	@FXML private TextField moneyPutIn;
-	
-	private CoffeeModel cc; // model
-	
-	public void initialize( ) {
-		cc = new CoffeeModel( );
-		
-		headerText.setFont(new Font(20));
-		go.setDisable(true);
-	}
-	
-	@FXML protected void setSelection( ActionEvent ae ) {
-		if( ae.getSource( ) == blackCoffee) {
-			cc.setCoffeeType( "Black"); 
-		} else if (ae.getSource( ) == coffeeWithSugar) {
-			cc.setCoffeeType( "Sugar");
-		} else if (ae.getSource( )  == coffeeWithSugarAndCreamer) {
-			cc.setCoffeeType( "Sugar and Creamer");
+	public class Controller {
+
+		private double amountNumber;
+			
+			@FXML private Button enter, go, cancel;
+			@FXML private RadioButton blackCoffee, coffeeWithSugar, coffeeWithSugarAndCreamer;
+			@FXML private Label headerText, currentSelection, amountPaid, change;
+			@FXML private TextField moneyPutIn;
+			
+			public static final NumberFormat MONEY = NumberFormat.getCurrencyInstance(); 
+			private Model cc; 
+			
+			public void initialize( ) {
+				cc = new Model( ); 
+				
+				headerText.setFont(new Font(20));
+				go.setDisable(true);
+				moneyPutIn.setDisable(true); 
+				amountPaid.setText(MONEY.format(amountNumber));
+				cancel.setDisable(true);
+				change.setText("");
+				enter.setDisable(true);
+				
+			}
+			
+			@FXML protected void setSelection( ActionEvent ae ) {
+				if( ae.getSource( ) == blackCoffee) {
+					cc.setCoffeeType( "Black"); 
+				} else if (ae.getSource( ) == coffeeWithSugar) {
+					cc.setCoffeeType( "Sugar");
+				} else if (ae.getSource( )  == coffeeWithSugarAndCreamer) {
+					cc.setCoffeeType( "Sugar and Creamer");
+				}
+				moneyPutIn.setDisable(false); 
+				enter.setDisable(false);
+			}
+			
+			@FXML protected void calculateAmountPaid( ActionEvent ae ) {
+				double price = .35;
+				String amount = moneyPutIn.getText()
+						amountNumber += Double.parseDouble( amount);
+
+				 if( amountNumber >= price){
+					 go.setDisable(false);	
+					 amountPaid.setText(MONEY.format(amountNumber)); 
+					 
+				 }
+			}
+			
+			@FXML protected void go( ActionEvent ae) {
+				
+				cc.calculateChange(amountNumber);
+				
+				change.setText(MONEY.format(cc.calculateChange(amountNumber)));
+				cancel.setDisable(false); 	
+			}
+			@FXML protected void reset(ActionEvent ae){
+				amountNumber = 0.00;
+				initialize();
+			}
+
 		}
-	}
-	
-	@FXML protected void calculateAmountPaid( ActionEvent ae ) {
-		double price = .35;
-		String amountPaid = moneyPutIn.getText();
-		 amountNumber += Double.parseDouble( amountPaid);
-
-		 if( amountNumber >= price){
-			 go.setDisable(false);			 
-		 }
-	}
-	
-	@FXML protected void go( ActionEvent ae) {
-		
-		cc.calculateChange(amountNumber);
-		
-	}
-
-}
